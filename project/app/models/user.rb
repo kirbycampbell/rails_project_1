3ec1b7dd8ea5e3ec40f702f02371abb409758ffa
Kirby <jkirbycampbell@gmail.com>
2018-10-04 19:34:18 -0700

    Updated Migrations

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :photos
+  has_many :topics
   has_many :statements, through: :topics
   has_many :photos, through: :topics
 end
--- project/db/migrate/20181004222004_create_topics.rb
+++ project/db/migrate/20181004222004_create_topics.rb
@@ -2,7 +2,8 @@ class CreateTopics < ActiveRecord::Migration[5.2]
   def change
     create_table :topics do |t|
       t.string :title
-      
+      t.belongs_to :user, index: true
+
 
       t.timestamps
     end
