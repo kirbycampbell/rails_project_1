b78af486da85112e4f498eb9fab8188b02d444c5
Kirby <jkirbycampbell@gmail.com>
2018-10-05 16:47:09 -0700

    statement section added

--- project/app/controllers/statements_controller.rb
+++ project/app/controllers/statements_controller.rb
@@ -1,16 +1,33 @@
 class StatementsController < ApplicationController
 
-  def new
+  def index
+    @statements = Statement.all
+  end
 
+  def new
+    @statement = Statement.new
+    #@topic = Topic.find(params[:id])
   end
 
   def create
 
+    @topic = Topic.find_by(params[:id])
+    @statement = Statement.new(statement_params)
+    @statement.topic = @topic
+    if @statement.save
+      redirect_to topic_path(@topic)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @statement = Statement.find(params[:statement_id])
   end
 
   private
 
   def statement_params
-
+    params.require(:statement).permit(:content)
   end
 end
